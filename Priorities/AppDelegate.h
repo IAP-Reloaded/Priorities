f150c66ab25cdbf513c5ee529dc4f34528604571
Hayden Harris Kaminsky <haykam821@icloud.com>
2015-12-13 10:18:22 -0500

    Initial Commit

---
+++ Priorities/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Priorities
+//
+//  Created by Randi Kaminsky on 12/13/15.
+//  Copyright © 2015 Hayden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Priorities/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Priorities
+//
+//  Created by Randi Kaminsky on 12/13/15.
+//  Copyright © 2015 Hayden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
